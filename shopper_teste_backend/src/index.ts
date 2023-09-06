d06801eae8ee5e71b3a0054264e55914a83ad7e9
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-09-06 15:25:44 -0300

    commit 4

--- shopper_teste_backend/src/index.ts
+++ shopper_teste_backend/src/index.ts
@@ -1,6 +1,7 @@
 import { AppDataSource } from "./data-source";
 import express from "express";
 import cors from "cors";
+import routes from "./routes";
 
 AppDataSource
   .initialize()
@@ -14,8 +15,8 @@ AppDataSource
 const app = express();
 app.use(express.json());
 app.use(cors);
-
-
+app.use(routes);
+app.listen(3000);
 
 /*
 import { AppDataSource } from "./data-source"
--- shopper_teste_backend/src/routes.ts
+++ shopper_teste_backend/src/routes.ts
@@ -1,4 +1,11 @@
 import { Router } from "express";
+import ProductController from "./controller/ProductController";
 
 const routes = Router();
+
+const product = new ProductController();
+routes.get("/product", product.listAll);
+routes.get("/product/:code", product.listByCode);
+routes.put("/product/:code", product.editPrice);
+
 export default routes;
